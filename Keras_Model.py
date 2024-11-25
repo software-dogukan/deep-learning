5ff4f9773957e75d6e72c86f5e83fd93ffb0c655
Dogukan Alkan <78702122+software-dogukan@users.noreply.github.com>
2024-11-25 17:57:36 +0300

    Update Keras_Model.py

--- Keras_Model.py
+++ Keras_Model.py
@@ -1,7 +1,5 @@
 import numpy as np
-import pandas as np
 import pandas as pd
-import tensorflow as tf
 import keras
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
@@ -12,6 +10,11 @@ y=data.iloc[:,-1].values
 x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.33,random_state=0)
 X_train=sc.fit_transform(x_train)
 X_test=sc.fit_transform(x_test)
-model=keras.Sequential()
-model.add(keras.layers.Dense(6,activation="relu",input_dim=11))
-model.add(keras.layers.Dense(6,activation="relu",input_dim=11))
+model=keras.models.Sequential()
+model.add(keras.layers.Dense(6,init="uniform",activation="relu",input_dim=11))
+model.add(keras.layers.Dense(3,init="uniform",activation="relu"))
+model.add(keras.layers.Dense(1,init="uniform",activation="sigmoid"))
+opt=keras.optimizers.Adam(learning_rate=0.01)
+loss_fn=keras.losses.SparseCategoricalCrossentropy()
+accuracy = keras.metrics.CategoricalAccuracy()
+model.compile(loss=loss_fn,optimizer=opt,metrics=accuracy)
